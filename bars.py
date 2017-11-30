9f844aafa4d5965ae89eea582b729b61613f278f
kporcelain <zhukovaksusha@gmail.com>
2017-11-30 12:24:46 +0300

    added output

--- bars.py
+++ bars.py
@@ -5,8 +5,8 @@ import sys
 
 def collect_data_from_json(json_data):
     with open(json_data, "r") as json_file:
-        list_of_bar_attributes = json.loads(json_file.read())
-    return list_of_bar_attributes["features"]
+        bars_attributes = json.loads(json_file.read())
+    return bars_attributes["features"]
 
 
 def get_seats_count_from_bar(bar):
@@ -21,15 +21,18 @@ def get_smallest_bar(bars):
     return min(bars, key=lambda x: get_seats_count_from_bar(x))
 
 
-def get_closest_from_me_bar(longtitude, latitude, input_longtitude, input_latitude):
-    return math.fabs(longtitude - input_longtitude) + math.fabs(latitude - input_latitude)
+def count_distance_between_two_bars(longitude, latitude, input_longitude, input_latitude):
+    return math.fabs(longitude - input_longitude) + math.fabs(latitude - input_latitude)
 
 
-def get_closest_bar(bars, longtitude, latitude):
-    return min(bars, key=lambda x: get_closest_from_me_bar(longtitude, latitude, x["geometry"]["coordinates"][0],
+def get_closest_bar(bars, longitude, latitude):
+    return min(bars, key=lambda x: count_distance_between_two_bars(longitude, latitude, x["geometry"]["coordinates"][0],
                                                            x["geometry"]["coordinates"][1]))
 
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         json_data = sys.argv[1]
+        print("Самый большой бар: ", get_biggest_bar(collect_data_from_json(json_data)))
+        print("Самый маленький бар: ", get_smallest_bar(collect_data_from_json(json_data)))
+        print("Самый ближайший бар: ", get_closest_bar(collect_data_from_json(json_data), float(input("Введите долготу: ", )), float(input("Введите широту: ",))))
