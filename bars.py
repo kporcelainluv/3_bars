c03de100a21850dd37270533ba7ab19bc30ce7bc
Ksusha Zhukova <zhukovaksusha@gmail.com>
2017-12-11 19:06:53 +0300

    updated output

--- bars.py
+++ bars.py
@@ -8,9 +8,17 @@ def collect_data_from_json(path_to_file):
         bars_data = json.loads(json_file.read())
     return bars_data["features"]
 
+def get_name_of_the_bar(bar):
+    return bar["properties"]['Attributes']["Name"]
+
+def get_address_of_the_bar(bar):
+    return bar["properties"]['Attributes']["Address"]
 
 def get_seats_count_from_bar(bar):
     return int(bar["properties"]['Attributes']["SeatsCount"])
+
+def get_phone_number_of_bar(bar):
+    return bar["properties"]['Attributes']["PublicPhone"][0]["PublicPhone"]
 
 
 def get_biggest_bar(bars):
@@ -27,19 +35,30 @@ def count_distance_between_user_and_bar(longitude, latitude, input_longitude, in
 
 def get_closest_bar(bars, longitude, latitude):
     return min(bars,
-               key=lambda x: count_distance_between_user_and_bar(longitude, latitude, x["geometry"]["coordinates"][0],
+               key=lambda x: count_distance_between_user_and_bar(longitude, latitude,
+                                                                 x["geometry"]["coordinates"][0],
                                                                  x["geometry"]["coordinates"][1]))
-
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         path_to_file = sys.argv[1]
         bars = collect_data_from_json(path_to_file)
-        print("Самый большой бар: ")
-        print(json.dumps(get_biggest_bar(bars), indent=2, sort_keys=True, ensure_ascii=False))
-        print("Самый маленький бар: ")
-        print(json.dumps(get_smallest_bar(bars), indent=2, sort_keys=True, ensure_ascii=False))
+        print()
+        print("Самый большой бар: ", get_name_of_the_bar(get_biggest_bar(bars)))
+        print("Количество мест: ", get_seats_count_from_bar(get_biggest_bar(bars)))
+        print("Адрес: ", get_address_of_the_bar(get_biggest_bar(bars)))
+        print("Телефон: ", get_phone_number_of_bar(get_biggest_bar(bars)))
+        print()
+        print("Самый маленький бар: ", get_name_of_the_bar(get_smallest_bar(bars)))
+        print("Количество мест: ", get_seats_count_from_bar(get_smallest_bar(bars)))
+        print("Адрес: ", get_address_of_the_bar(get_smallest_bar(bars)))
+        print("Телефон: ", get_phone_number_of_bar(get_smallest_bar(bars)))
+        print()
         longitude = float(input("Введите вашу долготу: ", ))
         latitude = float(input("Введите вашу широту: ", ))
-        print("Самый ближайший бар: ")
-        print(json.dumps(get_closest_bar(bars, longitude, latitude), indent=2, sort_keys=True, ensure_ascii=False))
+        print()
+        print("Самый ближайший бар: ", get_name_of_the_bar(get_closest_bar(bars, longitude, latitude)))
+        print("Количество мест: ", get_seats_count_from_bar(get_closest_bar(bars, longitude, latitude)))
+        print("Адрес: ", get_address_of_the_bar(get_closest_bar(bars, longitude, latitude)))
+        print("Телефон: ", get_phone_number_of_bar(get_closest_bar(bars, longitude, latitude)))
+
