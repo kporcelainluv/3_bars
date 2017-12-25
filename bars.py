86960b1245912cc6e31c78c273471ac8a775af0d
Ksusha Zhukova <zhukovaksusha@gmail.com>
2017-12-25 16:32:43 +0300

    simplified function, pep8 update

--- bars.py
+++ bars.py
@@ -9,6 +9,25 @@ def collect_data_from_json(path_to_file):
     return bars_data["features"]
 
 
+def get_biggest_bar(bars):
+    return max(bars, key=lambda x: get_seats_count_from_bar(x))
+
+
+def get_smallest_bar(bars):
+    return min(bars, key=lambda x: get_seats_count_from_bar(x))
+
+
+def get_closest_bar(bars, long, lat):
+    def distance(bars):
+        input_long = bars["geometry"]["coordinates"][0]
+        input_lat = bars["geometry"]["coordinates"][1]
+        dist_user_and_bar = math.fabs(long - input_long) + math.fabs(lat - input_lat)
+        return dist_user_and_bar
+
+    closest_bar = min(bars, key=distance)
+    return closest_bar
+
+
 def get_name_of_the_bar(bar):
     return bar["properties"]['Attributes']["Name"]
 
@@ -25,21 +44,11 @@ def get_phone_number_of_bar(bar):
     return bar["properties"]['Attributes']["PublicPhone"][0]["PublicPhone"]
 
 
-def get_biggest_bar(bars):
-    return max(bars, key=lambda x: get_seats_count_from_bar(x))
-
-
-def get_smallest_bar(bars):
-    return min(bars, key=lambda x: get_seats_count_from_bar(x))
-
-
-def distance_between_user_and_bar(long, lat, input_long, input_lat):
-    return math.fabs(long - input_long) + math.fabs(lat - input_lat)
-
-
-def get_closest_bar(bars, long, lat):
-    return min(bars, key=lambda x:
-        distance_between_user_and_bar(long, lat, x["geometry"]["coordinates"][0], x["geometry"]["coordinates"][1]))
+def print_bar_info(bar):
+    print("Количество мест: ", get_seats_count_from_bar(bar))
+    print("Адрес: ", get_address_of_the_bar(bar))
+    print("Телефон: ", get_phone_number_of_bar(bar))
+    return "   "
 
 
 if __name__ == '__main__':
@@ -48,22 +57,15 @@ if __name__ == '__main__':
         bars = collect_data_from_json(path_to_file)
         biggest_bar = get_biggest_bar(bars)
         smallest_bar = get_smallest_bar(bars)
-        print()
+
         print("Самый большой бар: ", get_name_of_the_bar(biggest_bar))
-        print("Количество мест: ", get_seats_count_from_bar(biggest_bar))
-        print("Адрес: ", get_address_of_the_bar(biggest_bar))
-        print("Телефон: ", get_phone_number_of_bar(biggest_bar))
-        print()
+        print(print_bar_info(biggest_bar))
         print("Самый маленький бар: ", get_name_of_the_bar(smallest_bar))
-        print("Количество мест: ", get_seats_count_from_bar(smallest_bar))
-        print("Адрес: ", get_address_of_the_bar(smallest_bar))
-        print("Телефон: ", get_phone_number_of_bar(smallest_bar))
-        print()
+        print(print_bar_info(smallest_bar))
+
         longitude = float(input("Введите вашу долготу: ", ))
         latitude = float(input("Введите вашу широту: ", ))
         closest_bar = get_closest_bar(bars, longitude, latitude)
-        print()
-        print("Самый ближайший бар: ", get_name_of_the_bar(closest_bar))
-        print("Количество мест: ", get_seats_count_from_bar(closest_bar))
-        print("Адрес: ", get_address_of_the_bar(closest_bar))
-        print("Телефон: ", get_phone_number_of_bar(closest_bar))
+
+        print("Ближайший бар: ", get_name_of_the_bar(closest_bar))
+        print(print_bar_info(closest_bar))
